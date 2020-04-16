779f7d3d273d88b90e8f03e2973966b8723cbc0f
EliotFerragni <eliot.ferragni@epfl.ch>
2020-04-16 09:08:10 +0200

    -Added makefile to include this library to a project

---
+++ threads_utilities.mk
@@ -0,0 +1,9 @@
+
+# Source files for the Threads utilities library
+THDUSRC = $(THDULIB)/threads_utilities.c
+
+# Include directories
+THDUINC = $(THDULIB)
+
+ALLCSRC += $(THDUSRC)
+ALLINC  += $(THDUINC)
